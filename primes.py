2ce5f1684281b29e547833e7c6ef5e3509208e64
Ahmed Osman <ahmed.osman@kcl.ac.uk>
2022-10-04 10:12:13 +0100

    Add solution

--- primes.py
+++ primes.py
@@ -3,20 +3,25 @@
 
 def primes(number_of_primes):
     list = []
-    
     #Case 1 : number of primes = 0
     #Case 2 : number of primes > 0
-
+    print('Working')
     if(number_of_primes>0):
         list.append(2)
 
+
         value = 3
-        isPrime = True
+
         while(len(list) < number_of_primes):
+            isPrime = True
             for num in  list :
                 if(value % num == 0):
                     isPrime = False
             if(isPrime):
                 list.append(value)
-            value += 1
+
+            value+=1
     return list
+
+
+print(primes(20))
