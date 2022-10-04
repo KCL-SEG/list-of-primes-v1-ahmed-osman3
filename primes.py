772cfbbcf25f6a18e84c66cd65756366ad6643e4
Ahmed Osman <ahmed.osman@kcl.ac.uk>
2022-10-04 09:40:02 +0100

    Add solution

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    
+    #Case 1 : number of primes = 0
+    #Case 2 : number of primes > 0
+
+    if(number_of_primes>0):
+        list.append(2)
+        
+        value = 3
+        while(len(list) < number_of_primes):
+            for num in  list :
+                if(value % num == 0):
+                    break
+                list.append(value)
     return list
